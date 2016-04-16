831fd822df2ca99d66a2cacc0585fa5353ee23ab
Ivy Ip <hkmg038836@gmail.com>
2016-04-16 19:28:20 +0800

    add some text and listener

--- BELLE/view2.lua
+++ BELLE/view2.lua
@@ -22,25 +22,38 @@ function scene:create( event )
 	bg:setFillColor( 1 )	-- white
 	
 	-- create some text
-	local title = display.newText( "Second View", 0, 0, native.systemFont, 32 )
+	local title = display.newText( "Diary", 0, 0, native.systemFont, 32 )
 	title:setFillColor( 0 )	-- black
 	title.x = display.contentWidth * 0.5
-	title.y = 125
+	title.y = 32
 	
-	local newTextParams = { text = "Loaded by the second tab's\n\"onPress\" listener\nspecified in the 'tabButtons' table", 
-							x = 0, y = 0, 
-							width = 310, height = 310, 
-							font = native.systemFont, fontSize = 14, 
-							align = "center" }
-	local summary = display.newText( newTextParams )
-	summary:setFillColor( 0 ) -- black
-	summary.x = display.contentWidth * 0.5 + 10
-	summary.y = title.y + 215
+	local addBackground = display.newRect( display.contentCenterX, 70, display.contentWidth * 0.8, 30 )
+	addBackground:setFillColor( 0 )	
+	local add = display.newText( "+", display.contentCenterX, 70, native.systemFont, 30 )
+	add:setFillColor( 1 )	
+
+
+	local tab1 = display.newText( "Routine", 0, 0, native.systemFont, 16 )
+	local tab2 = display.newText( "Weather", 0, 0, native.systemFont, 16 )
+	local tab3 = display.newText( "Satisfaction", 0, 0, native.systemFont, 16 )
+
+	tab1.x = display.contentWidth * 0.2
+	tab2.x = display.contentWidth * 0.5
+	tab3.x = display.contentWidth * 0.8
+	tab1.y = 110
+	tab2.y = 110
+	tab3.y = 110
+	tab1:setFillColor( 0 )
+	tab2:setFillColor( 0 )
+	tab3:setFillColor( 0 )
 	
 	-- all objects must be added to group (e.g. self.view)
 	sceneGroup:insert( bg )
 	sceneGroup:insert( title )
-	sceneGroup:insert( summary )
+	sceneGroup:insert( tab1 )
+	sceneGroup:insert( tab2 )
+	addBackground:addEventListener( "tap", addList )
+
 end
 
 function scene:show( event )
@@ -82,6 +95,9 @@ end
 
 ---------------------------------------------------------------------------------
 
+function addList()
+	print( "object tapped")
+end
 -- Listener setup
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
