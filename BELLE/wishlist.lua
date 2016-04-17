f5974c33c1cf70d7f7b8200dccba5a38cb9b6bdc
Lily Yung <ymll@live.hk>
2016-04-17 12:52:56 +0900

    Add wishlist and go back

--- BELLE/wishlist.lua
+++ BELLE/wishlist.lua
@@ -11,11 +11,32 @@ local composer = require( "composer" )
 -- Load scene with same root filename as this file
 local scene = composer.newScene( sceneName )
 
+_G.isWishlistLastItemVisible = false
+
 -------------------------------------------------------------------------------
 
 function onButtonAddClicked( event )
     if event.phase == "ended" then
         composer.gotoScene( "wishlistAdd" )
+    end
+end
+
+function scene:changeLastItem()
+    local buttonBorder = self:getObjectByName("button1_copy001")
+    local text = self:getObjectByName("Text_copy001")
+    local text2 = self:getObjectByName("Text3_copy001")
+    local color = self:getObjectByName("Circle_copy001")
+
+    if _G.isWishlistLastItemVisible then
+        transition.fadeIn( buttonBorder, {time = 500, transition = easing.inQuint} )
+        transition.fadeIn( text, {time = 500, transition = easing.inQuint} )
+        transition.fadeIn( text2, {time = 500, transition = easing.inQuint} )
+        transition.fadeIn( color, {time = 500, transition = easing.inQuint} )
+    else
+        buttonBorder.alpha = 0
+        text.alpha = 0
+        text2.alpha = 0
+        color.alpha = 0
     end
 end
 
@@ -36,6 +57,7 @@ function scene:show( event )
     local phase = event.phase
 
     if phase == "will" then
+        scene:changeLastItem()
         -- Called when the scene is off screen and is about to move on screen
     elseif phase == "did" then
         -- Called when the scene is now on screen
